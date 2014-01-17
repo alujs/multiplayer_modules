e6ce4f617ea5074dea75a90ca6c1865e9cd9b942
steinernein <steinernein@gmail.com>
2014-01-16 21:08:18 -0800

    working on manager then integrating - no tests

---
+++ spec/manager-spec.js
@@ -0,0 +1,11 @@
+var Rooms = require("../lib/manager.js");
+
+
+describe("Manager", function () {
+
+  xit("It should not overwrite old data ", function () {
+    room_list = new Rooms(socketid, 'dolem', init_data);
+
+  });
+
+});  
--- spec/sync-spec.js
+++ spec/sync-spec.js
@@ -1,4 +1,4 @@
-var gs = require('../lib/sync.js');
+var Sync = require('../lib/sync.js');
 var gamestate;
 
 var io = {};
@@ -19,9 +19,30 @@ var sockets = {};
 sockets.broadcast = {};
 sockets.broadcast.to = io.sockets.socket; // game Room w/o sender
 
+var packet = {
+  name: 'Bob',
+  time: new Date().getTime(),
+  lVeloc: [3,3,3],
+  pos: [0,0,0]
+};
+
+var falsepacket = {
+  name: 'Bob',
+  time: new Date().getTime() + 3000,
+  lVeloc: [3,3,3],
+  pos: [3330,33330,33330]
+};
+
+var truthpacket = {
+  name: 'Bob',
+  time: new Date().getTime() + 1000,
+  lVeloc: [3,3,3],
+  pos: [3,3,3]
+};
 
 describe("Sync", function () {
 	beforeEach(function() {
+    gamestate = new Sync('dolem', 1000, 4, 3000);
 	});
 
 	afterEach(function() {
@@ -30,20 +51,25 @@ describe("Sync", function () {
 
 
   it("It should initialize ", function() {
-    gamestate = new gs(io, 'dolem');
     expect(gamestate.name).toEqual('dolem');
-    expect(Object.keys(gamestate.io).length).toEqual(1);
+    expect(gamestate.cycle).toEqual(1000);
+    expect(gamestate.tol).toEqual(4);
   });
 
-  it("It should maintain the game state", function() {
-
+  it("It should reject values out of tolerance and init", function() {
+    gamestate.setInit(packet);
+    var result = gamestate.setMove(falsepacket);
+    expect(result).toBe(false)
   });
 
-  xit("It should force to sync ", function() {
+  xit("It should call to sync ", function() {
+    // need to attach spies at some point
   });
 
-  xit("It should write to the database", function() {
-
+  it("It should accept truthy values", function() {
+    gamestate.setInit(packet);
+    var result = gamestate.setMove(truthpacket);
+    expect(result).toBe(true)
   });
 
 });  
